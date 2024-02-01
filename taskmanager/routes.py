f1cb533bf9e033859dbb909c2e32aa1f4cd32261
Richard Marles <marleswebinfo@gmail.com>
2024-02-01 10:18:03 +0100

    First commit creating db using python

---
+++ taskmanager/routes.py
@@ -0,0 +1,10 @@
+""" Importamos lo que necesitamos """
+from flask import render_template
+from taskmanager import app, db 
+from taskmanager.models import Category, Task 
+
+
+@app.route("/")
+def home():
+    """ renderizamos base html """
+    return render_template("base.html")
