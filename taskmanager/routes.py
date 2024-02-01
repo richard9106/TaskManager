db45951b459adb0e8a7e65a755341be2747e7bf7
Richard Marles <marleswebinfo@gmail.com>
2024-02-01 12:18:24 +0100

    adding funtion to get the name o the category in routs.py

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,5 +1,5 @@
 """ Importamos lo que necesitamos """
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db 
 from taskmanager.models import Category, Task 
 
@@ -8,3 +8,20 @@ from taskmanager.models import Category, Task
 def home():
     """ renderizamos base html """
     return render_template("task.html")
+
+
+@app.route("/categories")
+def categories():
+    """render template categories"""
+    return render_template("categories.html")
+
+
+@app.route("/add_categories", methods=["GET", "POST"])
+def add_categories():
+    """render template categories"""
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
