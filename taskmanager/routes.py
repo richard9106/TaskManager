456ff1ac24c1dc848b66de79c4cdf9a4f18843b8
Richard Marles <marleswebinfo@gmail.com>
2024-02-02 11:31:51 +0100

    adding edit feature for tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -67,3 +67,18 @@ def add_task():
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("add_task.html", categories=thecategories)
+
+@app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    """render template categories"""
+    task = Task.query.get_or_404(task_id)
+    thecategories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task.task_name = request.form.get("task_name")
+        task.task_description = request.form.get("task_description")
+        task.is_urgent = bool(True if request.form.get("is_urgent") else False)
+        task.due_date = request.form.get("due_date")
+        task.category_id = request.form.get("category_id")
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit_task.html", task=task, categories=thecategories)
