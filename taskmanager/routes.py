aa11a86a3758514f29d67ccdbdcf231614525837
Richard Marles <marleswebinfo@gmail.com>
2024-02-02 12:04:25 +0100

    add requirements and procfile

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -82,3 +82,14 @@ def edit_task(task_id):
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("edit_task.html", task=task, categories=thecategories)
+
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    """render template home"""
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+   
